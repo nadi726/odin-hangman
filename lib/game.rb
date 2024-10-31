5a4c02392d4916ebafa14eef59f4298eefd4ac88
nadi726 <16650084+nadi726@users.noreply.github.com>
2024-10-31 11:51:59 +0200

    Add game loading feature & comments

--- lib/game.rb
+++ lib/game.rb
@@ -5,42 +5,61 @@ require_relative 'save_manager'
 
 # Main game class
 class Game
-  attr_reader :turn, :answer, :guess_so_far, :guessed_letters
+  attr_accessor :turn, :answer, :guess_so_far, :guessed_letters
 
-  TURNS = 7
+  TURNS = 10
   WORDS_FILE_PATH = 'google-10000-english-no-swears.txt'
   WORD_LENGTH_RANGE = (5..12).freeze
 
   def initialize
+    loaded_game = SaveManager.load_game
+    if loaded_game
+      make_game_from_hash loaded_game
+      puts "loaded game.\n\n"
+    else
+      make_new_game
+    end
+    @game_status = :ongoing # game_status is one of: :ongoing, :win, :lose, :saved
+  end
+
+  # setup the game state given a hash
+  def make_game_from_hash(hash)
+    attributes = %i[turn answer guess_so_far guessed_letters]
+    attributes.each do |attribute|
+      instance_variable_set("@#{attribute}", hash[attribute])
+    end
+  end
+
+  # setup a new game state
+  def make_new_game
+    puts "Making a new game.\n\n"
     @turn = 0
     @answer = make_answer
     @guess_so_far = Array.new(@answer.length, '-')
     @guessed_letters = []
-    @game_status = :ongoing # game_status is one of: :ongoing, :win, :lose, :saved
   end
 
+  # Main method. Run it to play the current game
   def play
     print "Game starts\n\n"
     play_turn while @game_status == :ongoing
     post_game
   end
 
+  # Play one turn in the game
   def play_turn
     @turn += 1
-    puts "Turn #{@turn}\nYour guess so far: #{@guess_so_far.join}"
-    update_guessed guess_letter
+    puts "Turn #{@turn}/#{TURNS}\nYour guess so far: #{@guess_so_far.join}"
+    update_guessed read_input
     check_game_status
     puts
   end
 
+  # Perform post-game operations, based on the game status
   def post_game
     if @game_status == :win || @game_status == :lose
-      puts "The asnwer was: #{@answer}"
-      if @game_status == :win
-        puts 'you win'
-      elsif @game_status == :lose
-        puts 'You lost'
-      end
+      puts "The answer was: #{@answer}"
+      puts @game_status == :win ? 'You win' : 'You lost'
     elsif @game_status == :saved
       SaveManager.save_game self
     end
@@ -57,13 +76,14 @@ class Game
     end
   end
 
-  def guess_letter
+  # Prompt the user for input until a valid input is given(either a letter or 'save')
+  def read_input
     loop do
-      guess = (Input.get 'Guess a letter: ').downcase
-      return if guess == 'save'
+      user_input = (Input.get 'Guess a letter: ').downcase
+      return if user_input == 'save'
 
-      validation_message = get_validation_message guess
-      return guess if validation_message.nil?
+      validation_message = get_validation_message user_input
+      return user_input if validation_message.nil?
 
       puts validation_message
     end
@@ -77,9 +97,11 @@ class Game
     end
   end
 
+  # Update to include the current guess
   def update_guessed(guess)
     if guess.nil?
       @game_status = :saved
+      @turn -= 1 # Saving shouldn't take a turn
       return
     end
 
@@ -89,6 +111,7 @@ class Game
     end
   end
 
+  # Update the game status if neccesary
   def check_game_status
     return if @game_status != :ongoing
 
--- lib/save_manager.rb
+++ lib/save_manager.rb
@@ -1,41 +1,101 @@
 # frozen_string_literal: true
 
 require 'json'
+require_relative 'input'
 
 # Save games and load saved games
 module SaveManager
   SAVE_FILE_PATH = 'saved_games.json'
 
-  def self.as_json(game)
-    {
-      answer: game.answer,
-      guess_so_far: game.guess_so_far,
-      guessed_letters: game.guessed_letters,
-      turn: game.turn - 1
-    }
+  # A bit excessive... Used to verify that the game object is valid
+  EXPECTED_KEYS = {
+    answer: ->(value, _) { value.is_a?(String) && value.match?(/[a-zA-Z]/) },
+    guess_so_far: ->(value, hash) { guess_so_far_valid?(value, hash) },
+    guessed_letters: ->(value, _) { value.is_a?(Array) },
+    turn: ->(value, _) { value.is_a?(Integer) && value >= 0 }
+  }.freeze
+
+  # convert game state to json
+  def self.game_as_json(game)
+    attributes = %i[answer guess_so_far guessed_letters turn]
+    attributes.to_h { |attr| [attr, game.send(attr)] }
   end
 
+  # add the game data to the save file
   def self.save_game(game)
     puts 'Saving game...'
     File.open(SAVE_FILE_PATH, 'r+') do |file|
-      contents = load_json file.read
+      contents = load_valid_games file.read
       file.rewind
-      contents.push as_json(game)
+      contents.push game_as_json(game)
       file.write JSON.pretty_generate(contents)
     end
+    puts 'Done.'
   end
 
-  def self.load_json(str)
+  # return an array of valid game states from the save file
+  def self.load_valid_games(str)
     begin
-      contents = JSON.parse str
+      contents = JSON.parse(str, symbolize_names: true)
     rescue JSON::ParserError
       contents = []
     end
-    contents.is_a?(Array) ? contents : []
+    contents.is_a?(Array) ? filter_valid_data(contents) : []
   end
 
-  # load the save file and prompt the user to choose a save file, if there are any
+  # load the save file and prompt the user to choose a saved game, if there are any
   def self.load_game
-    puts 'hiya'
+    games = load_valid_games(File.read(SAVE_FILE_PATH))
+    return if games.empty?
+    return unless load_saved_game?
+
+    choose_game games
+  end
+
+  # Only include saved games with valid states
+  def self.filter_valid_data(contents)
+    contents.select do |game|
+      EXPECTED_KEYS.all? do |key, lambda|
+        game.include?(key) && lambda.call(game[key], game)
+      end
+    end
+  end
+
+  # A helper method for EXPECTED_KEYS
+  def self.guess_so_far_valid?(guess, hash)
+    answer = hash[:answer]
+    guess.is_a?(Array) && guess.length == answer.length &&
+      guess.each_index.all? { |i| guess[i] == '-' || guess[i] == answer[i] }
+  end
+
+  def self.load_saved_game?
+    response = Input.get('Do you want to load a saved game(y/N)? ')
+    true if response.downcase == 'y'
+  end
+
+  # determine the game the user wants to load out of all saved games
+  def self.choose_game(games)
+    return games[0] if games.length == 1
+
+    loop do
+      response = determine_game_choice(games.length - 1)
+      return games[response] if response
+    end
+  end
+
+  # Return a choice for a valid game entry in the games array, or nil
+  def self.determine_game_choice(max_choice)
+    response = Input.get "Choose a game to load[0-#{max_choice}]: "
+
+    begin
+      response = Integer response
+    rescue ArgumentError
+      puts 'Please enter a valid number.'
+      return
+    end
+
+    return response if response.between?(0, max_choice)
+
+    puts 'Please enter an integer in range.'
   end
 end
